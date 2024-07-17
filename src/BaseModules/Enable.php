b99cd3944a7736617d2c05879bea0571b1f9c37f
Gurdeep Singh (Guru) <oyeaussie@gmail.com>
2024-07-18 02:46:31 +1000

    - Composer check if the installed plugins or modules have any updates available.

--- src/BaseModules/Enable.php
+++ src/BaseModules/Enable.php
@@ -132,6 +132,77 @@ class Enable extends Modules
         return true;
     }
 
+    protected function composerCheck($args)//Check for latest release
+    {
+        if (!isset($args[0])) {
+            $this->terminal->addResponse('Please provide needs to be checked, plugins or modules.', 1);
+
+            return false;
+        }
+
+        \cli\line("");
+        \cli\line('%bChecking ' . $args[0] . ' for any updates...%w');
+        \cli\line("");
+
+        if (strtolower($args[0]) === 'plugins') {
+            if (!isset($this->terminal->config['plugins']) ||
+                (isset($this->terminal->config['plugins']) && count($this->terminal->config['plugins']) === 0)
+            ) {
+                \cli\line("");
+                \cli\line("%yNo plugins available. Search plugins via composer in enable mode.%w");
+                \cli\line("Run command : composer search plugins");
+                \cli\line("");
+                \cli\line("%yTo install new plugins via composer in config mode.%w");
+                \cli\line("Run command : composer install plugin {plugin_name}");
+                \cli\line("");
+
+                return true;
+            }
+        }
+
+        if (isset($this->terminal->config[strtolower($args[0])]) && count($this->terminal->config[strtolower($args[0])]) > 0) {
+            foreach ($this->terminal->config[strtolower($args[0])] as $package) {
+                if (strtolower($args[0]) === 'modules' &&
+                    $package['package_name'] === 'phpterminal/phpterminal' &&
+                    !$this->terminal->viaComposer
+                ) {
+                    continue;
+                }
+
+                if ($this->runComposerCommand('show -a -l -f json ' . $package['package_name'])) {
+                    $composerInfomation = file_get_contents(base_path('composer.install'));
+
+                    $composerInfomation = trim(preg_replace('/<warning>.*<\/warning>/', '', $composerInfomation));
+
+                    $composerInfomation = @json_decode($composerInfomation, true);
+
+                    if ($composerInfomation && count($composerInfomation) > 0) {
+                        if (isset($composerInfomation['latest']) &&
+                            $composerInfomation['latest'] !== $package['version']
+                        ) {
+                            \cli\line('%yUpdate available for package %w' . $package['package_name']);
+                            \cli\line('%bInstalled version: %w' . $package['version']);
+                            \cli\line('%bAvailable version: %w' . $composerInfomation['latest']);
+                            \cli\line("");
+                        }
+                    } else {
+                        $this->readComposerInstallFile(true);
+
+                        return false;
+                    }
+                } else {
+                    $this->readComposerInstallFile(true);
+
+                    return false;
+                }
+            }
+        }
+
+        $this->terminal->addResponse('All installed packages are up to date!');
+
+        return true;
+    }
+
     public function getCommands() : array
     {
         return
@@ -196,6 +267,12 @@ class Enable extends Modules
                     "description"   => "Search modules via composer.",
                     "function"      => "composer"
                 ],
+                [
+                    "availableAt"   => "enable",
+                    "command"       => "composer check",
+                    "description"   => "composer check {plugins/modules}. Check if installed plugins/modules have any updates.",
+                    "function"      => "composer"
+                ]
             ];
     }
 }
